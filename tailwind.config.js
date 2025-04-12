dcfb5e744ea38ec80e6ed08b3bcc90e30d706853
Karolis Cibiras <83600971+Jeshki@users.noreply.github.com>
2025-04-12 12:43:56 +0300

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,17 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
-  darkMode: 'class',
-    theme: {
+  darkMode: 'class', // or 'media' based on your preference
+  theme: {
     extend: {
+      colors: {
+        'very-dark-gray': '#1a1a1a', // Custom very dark gray
+      },
       fontFamily: {
         serif: ['"Playfair Display"', 'serif'],
+        sans: ['"Cinzel"', 'sans-serif'], // You might want to set Cinzel as default sans if used widely
       },
     },
   },
+  plugins: [],
 }
